290201a2382014664ca5a0c1b84bd31c6079bd1b
sauravpanchal <saurav.panchal.08@gmail.com>
2022-04-21 10:26:08 +0530

    Update

--- r.py
+++ r.py
@@ -1,25 +1,20 @@
-# IU1941230093 Saurav Panchal
-# To implement RSA Algorithm
-
-# Import required modules
 import math
 
-# Convenience Functions
+from RSA import modify_rsa
+
 def calculate_gcd(e, phi):
     return math.gcd(e, phi)
 
 def check_prime(n):
     if n > 1:
-        for _ in range(2, n//2 + 1):
-            if n % _ == 0:
+        for i in range(2, n // 2 + 1):
+            if n % i == 0:
                 return False
         else:
             return True
     else:
         return False
 
-
-# Driver Functions
 def get_prime():
     while True:
         inp = input("Enter prime numbers ? ").split()
@@ -28,15 +23,14 @@ def get_prime():
             print("p, q (Accepted)")
             break
         else:
-            print("Try Again ...")
+            print("Try again ...")
     return p, q
-
 def calculate_n(p, q):
     return p * q
 
 def calculate_phi_of_n(p, q):
     phi = (p - 1) * (q - 1)
-    print("phi_of_n (Calculated) =>", phi)
+    print("phi_of_n (Calculated) => ", phi)
     return phi
 
 def get_e(phi_of_n):
@@ -44,43 +38,41 @@ def get_e(phi_of_n):
     if e > 1 and e < phi_of_n:
         gcd = calculate_gcd(e, phi_of_n)
         if gcd == 1:
-            print("e (Accepted)")
+            print("e (acc)")
             return e
         else:
-            print("e (Rejected) => gcd != 1")
+            print("e (rej)")
     else:
-        print("e (Rejected) => e < 1 or e > phi_of_n")
-
+        print("e (rej)")
+        
 def get_d(e, phi_of_n):
     d = None
     for i in range(50):
         d = round(((((phi_of_n) * i) + 1) / e), 2)
         if d > 1 and d / int(d) == 1.0:
-            print("d (Calculated) =>", d)
+            print("d", d)
             return int(d)
 
 def get_keys(e, d, n):
     public_key, private_key = (e, n), (d, n)
-    print("Public key =>", public_key)
-    print("Private key =>", private_key)
+    print("Pub", public_key)
+    print("Pri", private_key)
 
 def get_cipher(P, e, n, p, q):
     if P < n:
         C = ((P ** e) % (n))
     C = modify_rsa(C, p, q)
-    print("After modification (Cipher) :", C)
+    print("Cipher:", C)
     return C
 
 def get_plain_text(C, d, n, p, q):
-    P = modify_rsa(C, p, q, flag = 1, y = C)
-    P = ((P ** d) % (n))
-    print("After modification (Plain) :", P)
+    P = modify_rsa(C, p, q, 1, C)
+    P = ((P ** d) % n)
+    print("PT ", P)
     return P
 
-
-# Modification over standard RSA.
 def modify_rsa(C, p, q, flag = 0, y = None):
-    base = (p + q) // 2
+    base = 10
     if flag == 1:
         x = math.pow(base, y)
         return int(x)
@@ -90,16 +82,16 @@ def modify_rsa(C, p, q, flag = 0, y = None):
 
 p, q = get_prime()
 n = calculate_n(p, q)
-phi_of_n = calculate_phi_of_n(p ,q)
-e = get_e(phi_of_n = phi_of_n)
-d = get_d(e, phi_of_n = phi_of_n)
+phi_of_n = calculate_phi_of_n(p, q)
+e = get_e(phi_of_n)
+d = get_d(e, phi_of_n)
 get_keys(e, d, n)
 
-flag = int(input("Enter to use RSA as in Key-Exchange-Mode (0) or Cipher-Text-Mode (1) ? "))
+flag = int(input("Mode:"))
 if not flag:
-    C = get_cipher(P = p, e = e, n = n, p = p, q = q)
-    P = get_plain_text(C, d = d, n = n, p = p, q = q)
+    C = get_cipher(p, e, n, p, q)
+    P = get_plain_text(C, d, n, p, q)
 else:
-    m = int(input("Enter m ? "))
-    C = get_cipher(m, e = e, n = n, p = p, q = q)
-    P = get_plain_text(C, d = d, n = n, p = p, q = q)
+    m = int(input("m ? "))
+    C = get_cipher(m, e, n, p, q)
+    P = get_plain_text(C, d, n, p, q)
